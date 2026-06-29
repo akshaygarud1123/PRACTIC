4288d59e863b827d6b51ce200857c890866f8dfc
Akshay <akshaygarud1123@gmail.com>
2026-06-29 12:08:50 +0530

    my prctice test

---
+++ tests/practice.spec.js
@@ -0,0 +1,34 @@
+const {test, expect}= require('@playwright/test');
+ 
+test('view the brand is display or not', async ({page})=>{
+
+   await page.goto("https://automationexercise.com/");
+
+//click on the product butten
+  await  page.locator("//a[@href='/products']").click()
+
+    //chech the brand are available or not 
+    const brand =page.locator("//h2[normalize-space()='Brands']")
+    await  expect (brand).toBeVisible();
+
+    //click on any brand name
+   await page.locator("//a[@href='/brand_products/Madame']").click()
+   const brandTitle = page.locator("//h2[normalize-space()='Brand - Madame Products']")
+   await expect(brandTitle).toBeVisible();
+
+   // check brand products are displayed or not
+   const brandProduct = page.locator(".product-image-wrapper").first();
+   await expect(brandProduct).toBeVisible();
+
+   // click on any other brand
+   await page.locator("//a[@href='/brand_products/Babyhug']").click();
+
+   // verify the user goes to the new brand page and sees products
+   await expect(page.locator("//h2[contains(text(),'Brand -')]")).toBeVisible();
+
+
+
+
+
+
+})
